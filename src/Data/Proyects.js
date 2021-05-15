5665888593ad8ea67b0b3eb211ebe0d764fe5383
SnowMoon272 <CastielAltair0027@outlook.com>
2021-05-15 04:46:23 -0500

    Listo por hoy

--- src/Data/Proyects.js
+++ src/Data/Proyects.js
@@ -27,4 +27,28 @@ export default [
     urlb: "https://www.google.com.mx",
     image: facebook,
   },
+  {
+    title: "Google",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium nisi tenetur eius, id non incidunt dolor reiciendis veritatis quibusdam vel neque, expedita eos ab a, culpa quisquam accusantium ad asperiores.",
+    urla: "https://www.google.com.mx",
+    urlb: "https://twitter.com/home?lang=es",
+    image: google,
+  },
+  {
+    title: "Twitter",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium nisi tenetur eius, id non incidunt dolor reiciendis veritatis quibusdam vel neque, expedita eos ab a, culpa quisquam accusantium ad asperiores.",
+    urla: "https://twitter.com/home?lang=es",
+    urlb: "https://www.google.com.mx",
+    image: twitter,
+  },
+  {
+    title: "Facebook",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium nisi tenetur eius, id non incidunt dolor reiciendis veritatis quibusdam vel neque, expedita eos ab a, culpa quisquam accusantium ad asperiores.",
+    urla: "https://www.facebook.com/",
+    urlb: "https://www.google.com.mx",
+    image: facebook,
+  },
 ];
--- src/pages/proyects.jsx
+++ src/pages/proyects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Card, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { BasicLayout } from "../layouts/BasicLayout.tsx";
 import proyects from "../Data/Proyects";
 import "./proyects.scss";
@@ -12,21 +12,19 @@ const Proyects = () => {
         <div className="ctner">
           {proyects.map((proyect, index) => {
             return (
-              <Col key={index} xs={12} sm={4} className="project">
-                <Card>
-                  <div className="image" style={{ backgroundImage: `url("${proyect.image}")` }} />
-                  <Card.Body>
-                    <Card.Title>{proyect.title}</Card.Title>
-                    <Card.Text>{proyect.description}</Card.Text>
-                    <a href={proyect.urla} target="_blank" rel="noopener noreferrer">
-                      <Button variant="primary">Ver Projecto</Button>
-                    </a>
-                    <a href={proyect.urlb} target="_blank" rel="noopener noreferrer">
-                      <Button variant="primary">Ver Codigo</Button>
-                    </a>
-                  </Card.Body>
-                </Card>
-              </Col>
+              <div key={index} className="project">
+                <div className="image" style={{ backgroundImage: `url("${proyect.image}")` }} />
+                <Card.Body className="Card-Body">
+                  <h2>{proyect.title}</h2>
+                  <Card.Text>{proyect.description}</Card.Text>
+                  <a href={proyect.urla} target="_blank" rel="noopener noreferrer">
+                    <button type="button">Ver Projecto</button>
+                  </a>
+                  <a href={proyect.urlb} target="_blank" rel="noopener noreferrer">
+                    <button type="button">Ver Codigo</button>
+                  </a>
+                </Card.Body>
+              </div>
             );
           })}
         </div>
