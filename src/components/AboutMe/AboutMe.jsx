7eeb664cc9226a67fe9b0e198208841d5ea2673a
Manuel Roberto Serrano Torres <CastielAltair0027@outlook.com>
2022-05-30 15:21:08 -0500

    Actualizacion AboutMe.jsx

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -6,12 +6,13 @@ import "./AboutMe.scss";
 const AboutMe = () => {
   return (
     <Container className="about-me">
-      <p>Cuento con una gran facilidad de palabra y experiencia trabajando bajo presión.
-Me considero una persona autodidacta, pero capaz de pedir ayuda cuando es
-necesario, he aprendido desarrollo web y me he preparado como desarrollador
-Front-end durante 3 años, por mis propios medios.
-Confío plenamente en mi capacidad para producir ideas innovadoras para resolver
-desafíos profesionales</p>
+      <p>
+        Cuento con una gran facilidad de palabra y experiencia trabajando bajo presión. Me considero
+        una persona autodidacta, pero capaz de pedir ayuda cuando es necesario, he aprendido
+        desarrollo web y me he preparado como desarrollador Front-end durante 3 años, por mis
+        propios medios. Confío plenamente en mi capacidad para producir ideas innovadoras para
+        resolver desafíos profesionales.
+      </p>
       <hr />
       <a href={CV} target="_blanck">
         <Button variant="primary">Descargar CV</Button>
